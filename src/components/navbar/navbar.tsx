42709a45f504e4ed96590a057ced2be0ed73bd3b
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-02-22 21:08:03 +0300

    configured tailwind

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -15,69 +15,23 @@ import "./navbar.css";
 
 const Navbar = (props: {}) => {
   return (
-    <AppBar
-      position="static"
-      color="primary"
-      sx={{ bgcolor: "background.default", mb: 3 }}
-    >
-      <Toolbar>
-        <IconButton size="large" edge="start" color="inherit" aria-label="logo">
-          🔥
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          AniDB
-        </Typography>
-        <Stack direction="row" spacing={2}>
-          <NavLink to="/planets">
-            <Button sx={{ color: "text.primary" }}>Planets</Button>
-          </NavLink>
-          <NavLink to="/people">
-            <Button sx={{ color: "text.primary" }}>People</Button>
-          </NavLink>
-          <NavLink to="/starships">
-            <Button sx={{ color: "text.primary" }}>Starships</Button>
-          </NavLink>
-
-          {/* <Button
-            color="inherit"
-            id="resources-button"
-            aria-controls={open ? "resources-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            endIcon={<KeyboardArrowDownIcon />}
-            onClick={handleClick}
-            sx={{ color: "text.primary" }}
-          >
-            Resources
-          </Button> */}
-          <Button sx={{ color: "text.primary" }}>Login</Button>
-        </Stack>
-        {/* <Menu
-          id="resources-menu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "right",
-          }}
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-          MenuListProps={{
-            "aria-labelledby": "resources-button",
-          }}
-        >
-          <MenuItem onClick={handleClose} sx={{ color: "text.primary" }}>
-            Blog
-          </MenuItem>
-          <MenuItem onClick={handleClose} sx={{ color: "text.primary" }}>
-            Podcast
-          </MenuItem>
-        </Menu> */}
-      </Toolbar>
-    </AppBar>
+    <nav>
+      <div className="flex flex-row mx-0 my-2 bg-gray-900 text-purple-600">
+        <ul className="flex flex-row">
+          <li>
+            <a className="inline" href="/people">
+              People
+            </a>
+          </li>
+          <li>
+            <a href="/planets">Planets</a>
+          </li>
+          <li>
+            <a href="/starships">Starships</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
